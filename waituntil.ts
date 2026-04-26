eb25fb77828cea3c2f384e7eac257b5a33527661
hiracra-creator <hiracra@gmail.com>
2026-04-26 14:44:41 +0900

    Refactor waitButtonA to waitUntil function

--- waituntil.ts
+++ waituntil.ts
@@ -1,16 +1,9 @@
 namespace WaitUntil {
-    /**
-     * Aボタンが押されるまで待つ
-     */
-    //% block="Aボタンが押されるまで待つ"
-    export function waitButtonA(): void {
-        let done = false
-
-        input.onButtonPressed(Button.A, function () {
-            done = true
-        })
-
-        while (!done) {
+    //% block="%condition まで待つ"
+    //% condition.shadow=logic_boolean
+    //% shim=TD_ID
+    export function waitUntil(condition: boolean): void {
+        while (!condition) {
             basic.pause(20)
         }
     }
