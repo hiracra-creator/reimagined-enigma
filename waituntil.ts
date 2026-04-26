95c107a6be1d691f2b99f5ff4c6899fa6a6ae750
hiracra-creator <hiracra@gmail.com>
2026-04-26 14:38:18 +0900

    Change waitUntil to use a condition function
    Refactor waitUntil function to accept a condition callback.

--- waituntil.ts
+++ waituntil.ts
@@ -1,17 +1,11 @@
 namespace WaitUntil {
-    let lastCondition = false
-
+    /**
+     * 条件が true になるまで待つ
+     */
     //% block="%condition まで待つ"
     //% condition.shadow=logic_boolean
-    export function waitUntil(condition: boolean): void {
-        control.inBackground(function () {
-            while (true) {
-                lastCondition = condition
-                basic.pause(20)
-            }
-        })
-
-        while (!lastCondition) {
+    export function waitUntil(condition: () => boolean): void {
+        while (!condition()) {
             basic.pause(20)
         }
     }
