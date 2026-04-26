b62f06db7c6dad334a588411f0948b544c63df5e
hiracra-creator <hiracra@gmail.com>
2026-04-26 13:57:51 +0900

    Update waituntil.ts

--- waituntil.ts
+++ waituntil.ts
@@ -10,24 +10,4 @@ namespace WaitUntil {
             basic.pause(20)
         }
     }
-
-    /**
-     * 条件が true になるまで待つ
-     */
-    //% block="条件が真になるまで待つ %condition"
-    export function waitUntil(condition: () => boolean): void {
-        while (!condition()) {
-            basic.pause(20)
-        }
-    }
-
-    /**
-     * ボタンが押されるまで待つ
-     */
-    //% block="ボタン %btn が押されるまで待つ"
-    export function waitUntilButtonPressed(btn: Button): void {
-        while (!input.buttonIsPressed(btn)) {
-            basic.pause(20)
-        }
-    }
 }
